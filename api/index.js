f9a638f7e7707e01cd5a199e48ca29aa6b4fc3cf
Mohamed <newmemomedo2018@gmail.com>
2025-09-20 03:24:48 +0000

    Fix serverless function: Convert to ES modules (.mjs) and update Vercel config

--- api/index.js
+++
@@ -1,7 +0,0 @@
-// Vercel serverless function entry point
-// This file proxies all requests to our compiled Express app
-
-const app = require('../dist/index.js');
-
-// Export the Express app as a serverless function
-module.exports = app;
